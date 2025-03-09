93ad1ee5b68292f174b9b9db353b4a98540ff744
Anurag Bhattacharjee <anuragbhattacharjee612@gmail.com>
2025-03-09 13:19:22 +0530

    complete

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
+import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import octokit from "./lib/octokit.js";
 
-dotenv.config();
 const app = express();
 app.use(
   cors({
